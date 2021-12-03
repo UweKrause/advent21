e7a9277997f5629bc215881b9b1bf9e61491f983
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-12-03 13:44:54 +0100

    solution 3_1

--- 3/1.py
+++ 3/1.py
@@ -1,29 +1,25 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    df = pd.read_csv("example", names=["input"], dtype=str)
+    df = pd.read_csv("input", names=["input"], dtype=str)
 
-    # one column for every character
-    df["a"] = df["input"].str[0]
-    df["b"] = df["input"].str[1]
-    df["c"] = df["input"].str[2]
-    df["d"] = df["input"].str[3]
-    df["e"] = df["input"].str[4]
+    col_length = len(df["input"][0])
 
-    # find the most common value in each column
-    a = df["a"].mode()[0]
-    b = df["b"].mode()[0]
-    c = df["c"].mode()[0]
-    d = df["d"].mode()[0]
-    e = df["e"].mode()[0]
+    # add one column for every character
+    for pos_str in range(col_length):
+        df[pos_str] = df["input"].str[pos_str]
 
-    # concat the
-    bits_most_common = f"{a}{b}{c}{d}{e}"
+    # find the most common bit in each column and add it to the string containing the most common bits
+    bits_most_common = ""
+    for pos_str in range(col_length):
+        bits_most_common += df[pos_str].mode()[0]
 
-    # "flip" "bits" (suggestions for solution that actually uses binary welcome!)
+    # "flip" "bits"
     bits_least_common = ''.join('1' if x == '0' else '0' for x in bits_most_common)
 
     rate_epsilon = int(bits_most_common, 2)
     rate_gamma = int(bits_least_common, 2)
 
-    print(rate_epsilon, rate_gamma, rate_epsilon * rate_gamma)
+    power_consumption = rate_epsilon * rate_gamma
+
+    print(power_consumption)  # 2498354
