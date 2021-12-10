585f0666aa43703f2be8933a519569b594c93223
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-12-10 08:52:48 +0100

    better solution, actually finishes quick

--- 6/2.py
+++ 6/2.py
@@ -1,30 +1,45 @@
 days_to_simulate = 256
 
 
-# solution takes ages (does it finish at all?)
-# TODO: implement better
-
-
 def main():
-    state = parse_input()
+    """
+    Instead of simulating all the fish,
+    this solution just keeps track of the actual number of fish and applies the given rules to them.
+
+    (There is a shorter way to write this, using something like `new_fish := old_fish.pop(0).)
+    """
+
+    fish_swarm = parse_input()
+
+    fish_count = {}
+
+    for timer in range(9):
+        fish_count[timer] = fish_swarm.count(timer)
 
     for day in range(1, days_to_simulate + 1):
-        pos = -1
-        for i in state.copy():
-            pos += 1
-            if i == 0:
-                state[pos] = 6
-                state.append(8)
-            else:
-                state[pos] -= 1
+        fish_count_zeroes = fish_count[0]
 
-        # print(f"after {day} days", state)
+        # each other number [1-8] decreases by 1 if it was present at the start of the day
+        fish_count[0] = fish_count[1]
+        fish_count[1] = fish_count[2]
+        fish_count[2] = fish_count[3]
+        fish_count[3] = fish_count[4]
+        fish_count[4] = fish_count[5]
+        fish_count[5] = fish_count[6]
+        fish_count[6] = fish_count[7]
+        fish_count[7] = fish_count[8]
 
-    print(len(state))  # 396210
+        # Each day, a 0 becomes a 6 and adds a new 8 to the end of the list
+        fish_count[6] += fish_count_zeroes
+        fish_count[8] = fish_count_zeroes
+
+        # print(f"day {day}: {sum(fish_count.values())}: {fish_count}")
+
+    print(sum(fish_count.values()))  # 1770823541496
 
 
 def parse_input():
-    with open("example") as lines:
+    with open("input") as lines:
         for line in lines:
             return [int(x) for x in line.strip().split(sep=",")]
 
